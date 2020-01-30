5c0fa18a2dd73884da3a677a210b86d43152f330
zassou <nakamura.k.bv@m.titech.ac.jp>
2020-01-30 20:06:46 +0900

    偽画像生成の際rgbを一度に生成できるようになっていなかったので修正

--- GAN.py
+++ GAN.py
@@ -78,7 +78,7 @@ def train_model(G,D,dataloader,num_epochs):
 			d_out_real = D(imgs)
 			#偽の画像を生成して判定
 			input_z = torch.randn(mini_batch_size,z_dim,3).to(device)
-			input_z = input_z.view(input_z.size(0),input_z.size(1),1,1)
+			input_z = input_z.view(input_z.size(0),input_z.size(1),1,3)
 			print(input_z.size())
 			fake_images = G(input_z)
 			#誤差の計算
@@ -95,8 +95,8 @@ def train_model(G,D,dataloader,num_epochs):
 			#generatorの学習
 			#-------------------------
 			#偽の画像を生成して判定
-			input_z = torch.randn(mini_batch_size,z_dim).to(device)
-			input_z = input_z.view(input_z.size(0),input_z.size(1),1,1)
+			input_z = torch.randn(mini_batch_size,z_dim,3).to(device)
+			input_z = input_z.view(input_z.size(0),input_z.size(1),1,3)
 			fake_images = G(input_z)
 			d_out_fake = D(fake_images)
 			#誤差の計算
