5ef5e943cea020ff35691382c4c9844cbe845e70
zassou <nakamura.k.bv@m.titech.ac.jp>
2020-01-30 18:03:15 +0900

    no message

---
+++ dataloader.py
@@ -0,0 +1,54 @@
+#encoding:utf-8
+
+from importer import *
+
+def make_datapath_list():
+	train_img_list = list()#画像ファイルパスのリストを作り、戻り値とする
+	for img_idx in range(0,102):
+		img_path = "./dataset/"+str(img_idx).zfill(3)+".jpg"
+		train_img_list.append(img_path)
+	return train_img_list
+
+class ImageTransform():
+	#画像の前処理クラス
+	def __init__(self,mean,std,resize_width_height_pixel):
+		self.data_transform = transforms.Compose([
+				transforms.Resize(resize_width_height_pixel),
+				transforms.ToTensor(),
+				transforms.Normalize(mean,std)
+			])
+	def __call__(self,img):
+		return self.data_transform(img)
+
+class GAN_Img_Dataset(data.Dataset):
+	#画像のデータセットクラス
+	def __init__(self,file_list,transform):
+		self.file_list = file_list
+		self.transform = transform
+	#画像の枚数を返す
+	def __len__(self):
+		return len(self.file_list)
+	#前処理済み画像の、Tensor形式のデータを取得
+	def __getitem__(self,index):
+		img_path = self.file_list[index]
+		img = Image.open(img_path)#[高さ][幅][RGB]
+		img_transformed = self.transform(img)
+		return img_transformed
+
+#動作確認
+train_img_list = make_datapath_list()
+
+mean = (0.5,)
+std = (0.5,)
+train_dataset = GAN_Img_Dataset(file_list=train_img_list,transform=ImageTransform(mean,std,resize_width_height_pixel=64))
+
+batch_size = 1
+train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
+
+batch_iterator = iter(train_dataloader)
+imgs = next(batch_iterator)
+print(imgs.size())
+
+
+
+
--- discriminator.py
+++ discriminator.py
@@ -1,31 +1,5 @@
 #encoding:utf-8
-
-import glob
-import os.path as osp
-import random
-import numpy as np
-import json
-from PIL import Image
-from tqdm import tqdm
-import matplotlib as mpl
-#mpl.use('Agg')# AGG(Anti-Grain Geometry engine)
-import matplotlib.pyplot as plt
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import torchvision
-from torchvision import models,transforms
-import torch.nn.init as init
-from torch.autograd import Function
-import torch.nn.functional as F
-
-import xml.etree.ElementTree as ET
-#import cv2
-from itertools import product
-from math import sqrt
-
+from importer import *
 from generator import *
 
 class Discriminator(nn.Module):
@@ -55,15 +29,15 @@ class Discriminator(nn.Module):
 		out = self.last(out);
 		return out
 
-#動作確認
-G = Generator(z_dim=20,image_size=64)
-D = Discriminator(z_dim=20,image_size=64)
+# #動作確認
+# G = Generator(z_dim=20,image_size=64)
+# D = Discriminator(z_dim=20,image_size=64)
 
-input_z = torch.randn(1,20)
-input_z = input_z.view(input_z.size(0),input_z.size(1),1,1)
-fakeimg = G(input_z)
+# input_z = torch.randn(1,20)
+# input_z = input_z.view(input_z.size(0),input_z.size(1),1,1)
+# fakeimg = G(input_z)
 
-d_out = D(fakeimg)
+# d_out = D(fakeimg)
 
-print(nn.Sigmoid()(d_out))
+# print(nn.Sigmoid()(d_out))
 
