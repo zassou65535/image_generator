cf6ded0316f9e5713e844db4c5f956c423a41e3c
zassou <nakamura.k.bv@m.titech.ac.jp>
2020-02-01 19:41:59 +0900

    動作確認できたのでnum_epochsを300に変更

--- GAN.py
+++ GAN.py
@@ -142,7 +142,7 @@ batch_size = 5
 train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
 
 #epoch数指定
-num_epochs = 1;
+num_epochs = 300;
 #モデルを学習させる
 G_update,D_update = train_model(G,D,dataloader=train_dataloader,num_epochs=num_epochs)
 
